dd2f0316a32f11d840f157c9a0d61b311be2d1b1
Christoffer Olesen <chrisolesen@Christoffers-MacBook-Air.local>
2024-02-09 11:45:21 -0500

    Adjust shadcn input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,7 +3,10 @@ import * as React from "react"
 import { cn } from "~/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+    customProp?: string; // Example of an additional property
+    // Add other properties if needed
+  }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
