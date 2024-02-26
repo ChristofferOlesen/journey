449502360735283c81c1b629d65a67432fa8bc29
Christoffer Olesen <chrisolesen@Christoffers-MacBook-Air.local>
2024-02-26 15:35:03 -0500

    Change background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
-import { Button } from "../components/ui/button"
-
+import { Button } from "../components/ui/button";
 
 import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
@@ -9,11 +8,13 @@ import { api } from "~/trpc/server";
 
 export default async function Home() {
   noStore();
-  const hello = await api.post.hello.query({ text: "Please sign in to use the app." });
+  const hello = await api.post.hello.query({
+    text: "Please sign in to use the app.",
+  });
   const session = await getServerAuthSession();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#F8FAFC] to-[#F1F5F9] text-slate-800">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-slate-50 to-slate-900 text-slate-800">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Customer <span className="text-[hsl(280,100%,70%)]">Journey</span>
@@ -21,12 +22,14 @@ export default async function Home() {
         <div className="flex flex-col items-center gap-2">
           <div className="flex flex-col items-center justify-center gap-4">
             <p className="text-center text-2xl text-slate-800">
-              {session ? (<span>Logged in as {session.user?.name}</span>) : (<span>{hello ? hello.greeting : "Loading..."}</span>)}
+              {session ? (
+                <span>Logged in as {session.user?.name}</span>
+              ) : (
+                <span>{hello ? hello.greeting : "Loading..."}</span>
+              )}
             </p>
             <Button asChild variant="outline">
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              >
+              <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
                 {session ? "Sign out" : "Sign in with Github"}
               </Link>
             </Button>
@@ -36,7 +39,6 @@ export default async function Home() {
         <CrudShowcase />
         <Customers />
         <Posts />
-
       </div>
     </main>
   );
@@ -69,7 +71,7 @@ async function Customers() {
 
   return (
     <div className="w-full max-w-xs">
-      {data?.map((customer) => (<div key={customer.id}>{customer.name}</div>))}
+      {data?.map((customer) => <div key={customer.id}>{customer.name}</div>)}
     </div>
   );
 }
@@ -83,11 +85,11 @@ async function Posts() {
   return (
     <div className="w-full max-w-xs">
       {data?.map((post) => (
-        <div key={post.id} className="mb-10" >
+        <div key={post.id} className="mb-10">
           <div>{post.name} </div>
           <div>Created: {post.createdAt.toLocaleString()}</div>
         </div>
-        ))}
+      ))}
     </div>
   );
 }
